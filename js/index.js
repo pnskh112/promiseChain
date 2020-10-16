c2a4a64194cd1c21df1a01ed76535512034e763e
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-10-16 21:44:07 +0900

    catchメソッドでpromiseを繋ぐ練習

--- js/index.js
+++ js/index.js
@@ -7,23 +7,23 @@ function testPromise1(input){
 console.log(testPromise1("太郎"));
 
 
-function testPromise2(pay){
-    return new Promise(ok => {
-        let change = pay - 100;
-        ok(change);
-    })
-}
+// function testPromise2(pay){
+//     return new Promise(ok => {
+//         let change = pay - 100;
+//         ok(change);
+//     })
+// }
 
-console.log(testPromise2(200));
+// console.log(testPromise2(200));
 
-testPromise2(400).then(change1 =>{
-    console.log(change1);
-    return testPromise2(change1); //Promiseでラップされた状態
-}).then(change2 =>{
-    console.log(change2);
-}).catch(e => {
-    console.log(e);
-});
+// testPromise2(400).then(change1 =>{
+//     console.log(change1);
+//     return testPromise2(change1); //Promiseでラップされた状態
+// }).then(change2 =>{
+//     console.log(change2);
+// }).catch(e => {
+//     console.log(e);
+// });
 
 function testPromise3(pay){
     return new Promise((ok,ng) => {
@@ -34,6 +34,11 @@ function testPromise3(pay){
         ng('お金がありません。恵んでください。');
     });
 }
-console.log(testPromise3(200));
-console.log(testPromise3(80).catch( e  =>{console.log(e)}));
-
+// console.log(testPromise3(200));
+// console.log(testPromise3(80).catch( e  =>{console.log(e)}));
+console.log(testPromise3(280).then(change1 => {
+    console.log(change1);
+    return testPromise3(change1);
+}).then(change2 => {
+    console.log(change2);
+}).catch(testPromise3( e => {console.log(e)})));
