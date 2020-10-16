5823b84ec4121322cf9ba6c25073e13547649088
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-10-16 21:17:50 +0900

    catchメソッドでpromiseを繋ぐ練習

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ console.log(testPromise1("太郎"));
 
 
 function testPromise2(pay){
-    return new Promise((ok) => {
+    return new Promise(ok => {
         let change = pay - 100;
         ok(change);
     })
@@ -16,9 +16,24 @@ function testPromise2(pay){
 
 console.log(testPromise2(200));
 
-testPromise2(400).then((change)=>{
-    console.log(change);
-    return testPromise2(change); //Promiseでラップされた状態
-}).then((change )=>{
-    console.log(change);
+testPromise2(400).then(change1 =>{
+    console.log(change1);
+    return testPromise2(change1); //Promiseでラップされた状態
+}).then(change2 =>{
+    console.log(change2);
+}).catch(e => {
+    console.log(e);
 });
+
+function testPromise3(pay){
+    return new Promise((ok,ng) => {
+        if (pay > 100){
+            let change = pay - 100;
+            ok(change);
+        }
+        ng('お金がありません。恵んでください。');
+    });
+}
+console.log(testPromise3(200));
+console.log(testPromise3(80).catch( e  =>{console.log(e)}));
+
