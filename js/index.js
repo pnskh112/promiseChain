3daaaaf51b5b41900ad26995ff2e3c8dcc2ea39b
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2020-10-16 20:43:15 +0900

    thenメソッドでpromiseを繋ぐ練習

--- js/index.js
+++ js/index.js
@@ -5,3 +5,20 @@ function testPromise1(input){
     })
 }
 console.log(testPromise1("太郎"));
+
+
+function testPromise2(pay){
+    return new Promise((ok) => {
+        let change = pay - 100;
+        ok(change);
+    })
+}
+
+console.log(testPromise2(200));
+
+testPromise2(400).then((change)=>{
+    console.log(change);
+    return testPromise2(change); //Promiseでラップされた状態
+}).then((change )=>{
+    console.log(change);
+});
